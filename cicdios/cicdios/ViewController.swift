ba67b91e54748eee59938db829f2705c4d2b7b10
Tamir Mualem <Tamir.Mualem@vonage.com>
2020-07-07 14:53:52 +0300

    First commit

---
+++ cicdios/cicdios/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cicdios
+//
+//  Created by Tamir Mualem on 07/07/2020.
+//  Copyright © 2020 Tamir Mualem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
